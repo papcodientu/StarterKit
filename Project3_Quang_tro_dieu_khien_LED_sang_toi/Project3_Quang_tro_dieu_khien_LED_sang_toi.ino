a0bde344ac446f0e7f8b2150dc6ca70ad334493c
PAP Co Dien Tu <papcodientu@gmail.com>
2015-03-12 09:26:31 +0700

    Thay doi project 2a, 2b va 3.

--- Project3_Quang_tro_dieu_khien_LED_sang_toi/Project3_Quang_tro_dieu_khien_LED_sang_toi.ino
+++ Project3_Quang_tro_dieu_khien_LED_sang_toi/Project3_Quang_tro_dieu_khien_LED_sang_toi.ino
@@ -14,6 +14,7 @@ Cách lắp mạch:
 Tác giả: Khang Nguyễn
 Ngày: 03/01/2015
 Lịch sử thay đổi
+- 12/03/2015 Rev. 1.1 - Map giá trị quang trở và giá trị xuất ra LED
 - 03/01/2015 Rev. 1.0
 Website: http://papcodientu.com/
 */
@@ -26,6 +27,7 @@ int quangTro = A0;
 
 // gia tri doc duoc tu bien tro 0-1023
 int giatriquangTro = 0;
+int giatridenLED = 0;
 
 void setup() {                
   // cài đặt cổng led là cổng output
@@ -36,8 +38,11 @@ void loop() {
   // đọc giá trị từ quang trở
   giatriquangTro = analogRead(quangTro);
   
+  // map giá trị quang trở với giá trị xuất ra LED
+  giatridenLED = map(giatriquangTro, 0, 1023, 0, 255);
+  
   // hiển thị độ sáng đèn LED theo giá trị quang trở
-  analogWrite(denLED, giatriquangTro/2);
+  analogWrite(denLED, giatridenLED/2);
   
   // độ trễ 100 mili giây để ổn định giá trị quang trở
   delay(100);
