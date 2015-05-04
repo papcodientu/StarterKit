eb523a37d7e090e99899bbbbe98485e121059e13
Co Dien Tu PAP <papcodientu@gmail.com>
2015-05-04 12:24:41 +0700

    Cap nhat code Project8b.

--- Project8b_Dieu_khien_coi_phat_dieu_nhac/Project8b_Dieu_khien_coi_phat_dieu_nhac.ino
+++ Project8b_Dieu_khien_coi_phat_dieu_nhac/Project8b_Dieu_khien_coi_phat_dieu_nhac.ino
@@ -15,15 +15,72 @@ Lịch sử thay đổi
 Website: http://papcodientu.com/
 */
 
-// khai báo cổng coi
-int coi = 9;
+#include "pitches.h"
 
-void setup() {                
-  // cài đặt cổng led là cổng output
+const int coi = 11;
+const int cambienPIR = 2;
+
+int melody[] = {
+  NOTE_C4, NOTE_D4,NOTE_E4, NOTE_C4, 
+  NOTE_C4, NOTE_D4,NOTE_E4, NOTE_C4,
+  0, 
+  NOTE_E4, NOTE_F4, NOTE_G4,
+  NOTE_E4, NOTE_F4, NOTE_G4,
+  0,
+  NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_C4,
+  NOTE_G4, NOTE_A4, NOTE_G4, NOTE_F4, NOTE_E4, NOTE_C4,
+  0,
+  NOTE_C4, NOTE_G4, NOTE_C4,
+  NOTE_C4, NOTE_G4, NOTE_C4,
+  0,
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  8, 8, 8, 8,
+  8, 8, 8, 8,
+  8,
+  8, 8, 8,
+  8, 8, 8,
+  8,
+  8, 8, 8, 8, 8, 8,
+  8, 8, 8, 8, 8, 8,
+  8,
+  8, 8, 8,
+  8, 8, 8
+};
+
+void setup() {
+  // coi la cong output
   pinMode(coi, OUTPUT);
+  
+  // cam bien PIR dau vao
+  pinMode(cambienPIR, INPUT);
 }
 
 void loop() {
-
+  // doc gia tri tu cam bien PIR
+  if (digitalRead(cambienPIR) == HIGH) {
+    choiNhac();
+  }
+  delay(10);
 }
 
+void choiNhac() {
+ // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < sizeof(melody); thisNote++) {
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(coi, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(coi);
+  } 
+}
